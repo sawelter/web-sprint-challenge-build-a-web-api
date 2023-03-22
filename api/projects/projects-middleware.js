a1e56b12c4d2d6bfbad44ba96a1a0d08ec22d913
Sawyer <sawyer.welter@gmail.com>
2023-03-22 15:08:42 -0500

    Projects get, post, put implemented

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,29 @@
 // add middlewares here related to projects
+const express = require('express');
+const Projects = require('./projects-model.js')
+
+
+async function checkProjectId(req, res, next) {
+    const { id } = req.params;
+    const project = await Projects.get(id);
+    if(project) {
+        req.project = project;
+        next();
+    } else {
+        res.status(404).json(`No projects with the given id`)
+    }
+}
+
+async function checkNewProject(req, res, next) {
+    const { name, description, completed } = req.body;
+    if(!name || !description) {
+        res.status(400).json(`Missing required text field`);
+    } else {
+        next();
+    }
+}
+
+module.exports = {
+    checkProjectId,
+    checkNewProject,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,31 +1,42 @@
 // Write your "projects" router here!
 const express = require('express');
-
 const router = express.Router();
+const Projects = require('./projects-model.js');
+const mid = require('./projects-middleware.js');
 
-router.get('/', (req, res) => {
-// Returns an array of projects as the body of the response.
-//If there are no projects it responds with an empty array.
+
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then(projects => {
+            res.json(projects);
+        })
+        .catch(next);
 })
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', mid.checkProjectId, (req, res, next) => {
+    res.status(200).json(req.project);
+})
+
+
+router.post('/', mid.checkNewProject, (req, res, next) => {
+    const { name, description } = req.body;
+    Projects.insert({name, description})
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(next);
+})
+
+
+router.put('/:id', [mid.checkProjectId, mid.checkNewProject], (req, res, next) => {
+    const { name, description } = req.body;
     const { id } = req.params;
-//Returns a project with the given id as the body of the response.
-// If there is no project with the given id it responds with a status code 404.
-})
-
-
-router.post('/', (req, res) => {
-// Returns the newly created project as the body of the response.
-// If the request body is missing any of the required fields it responds with a status code 400.
-})
-
-
-router.put('/:id', (req, res) => {
-// Returns the updated project as the body of the response.
-// If there is no project with the given id it responds with a status code 404.
-// If the request body is missing any of the required fields it responds with a status code 400.
+    Projects.update(id, {name, description})
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(next);
 })
 
 router.delete('/:id', (req, res) => {
